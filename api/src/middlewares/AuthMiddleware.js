c63c9a5898c5671af28469fd10607b85c9c9f391
Herick Vinicius <herick.gustavo@gmail.com>
2021-06-28 00:03:03 -0400

    refactor: change the error handling on the middleware.

--- api/src/middlewares/AuthMiddleware.js
+++ api/src/middlewares/AuthMiddleware.js
@@ -1,30 +1,32 @@
 const jwt = require("jsonwebtoken");
+const errors = require("../helpers/errors");
 
 module.exports = (req, res, next) => {
   try {
     const authHeader = req.headers.authorization;
     if (!authHeader) {
-      throw new Error("No token provided");
+      errors.forbiddenResponse(res, "no token provided");
     }
 
     const parts = authHeader.split(" ");
     if (!parts.length === 2) {
-      throw new Error("Token error");
+      errors.unprocessableEntityResponse(res, "token error");
     }
 
     const [schema, token] = parts;
     if (!/^Bearer$/i.test(schema)) {
+      errors.unprocessableEntityResponse(res, "malformated token");
       throw new Error("Malformatted token");
     }
 
     jwt.verify(token, process.env.JWT_SALT, (err, decoded) => {
       if (err) {
-        throw new Error("Invalid token");
+        errors.unprocessableEntityResponse(res, "invalid token");
       }
       req.userId = decoded.userId;
       return next();
     });
   } catch (error) {
-    return res.status(500).send({ error: error.message });
+    errors.unhandledResponse(res, error.message);
   }
 };
